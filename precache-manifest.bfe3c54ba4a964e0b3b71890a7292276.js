4b2141c8b1ddcd8d7a4fff2debb3c5bb9a6b246a
Milton Boos Junior <junior_boos@live.com>
2020-07-16 20:26:20 +0100

    Updates

---
+++ precache-manifest.bfe3c54ba4a964e0b3b71890a7292276.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565ea5331589028eb31c6251c0a5531c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2c7d307ada0ce51267ec",
+    "url": "/portfolio/static/css/main.46fa582c.chunk.css"
+  },
+  {
+    "revision": "f1a8c2e1bb15e7e6ccda",
+    "url": "/portfolio/static/js/2.2d8bd9e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.2d8bd9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7d307ada0ce51267ec",
+    "url": "/portfolio/static/js/main.10a08e2b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "de134bef88bacd22e7419965b6ff312d",
+    "url": "/portfolio/static/media/arbeit.de134bef.png"
+  },
+  {
+    "revision": "006caf8bc5f3b93730e05f61fae81a92",
+    "url": "/portfolio/static/media/at-sign.006caf8b.svg"
+  },
+  {
+    "revision": "d7ef8adba1aa16a74b91b787cde50f45",
+    "url": "/portfolio/static/media/code.d7ef8adb.svg"
+  },
+  {
+    "revision": "ea244d8ab1cec6cd9027a7c367a7e9de",
+    "url": "/portfolio/static/media/computer.ea244d8a.png"
+  },
+  {
+    "revision": "ddfe036627841f454a75d05b0de59eb8",
+    "url": "/portfolio/static/media/email-marketing.ddfe0366.png"
+  },
+  {
+    "revision": "bf5cd829572ac9f00c3ffa35d7ee533b",
+    "url": "/portfolio/static/media/facebook.bf5cd829.png"
+  },
+  {
+    "revision": "5cff78e44ca42e5026f832f7b41fb018",
+    "url": "/portfolio/static/media/github.5cff78e4.svg"
+  },
+  {
+    "revision": "2e9d982113e8a8033c6c284eecd77eff",
+    "url": "/portfolio/static/media/instagram.2e9d9821.png"
+  },
+  {
+    "revision": "2f7efb61ad1da6dc68f43a9ce8d8840a",
+    "url": "/portfolio/static/media/klabin.2f7efb61.png"
+  },
+  {
+    "revision": "5178c29195aedd533d1f2a9173ba2cd8",
+    "url": "/portfolio/static/media/linkedin.5178c291.png"
+  },
+  {
+    "revision": "20e8982ec7e03638afe27f5c3e897681",
+    "url": "/portfolio/static/media/mail.20e8982e.svg"
+  },
+  {
+    "revision": "276c5b05ff6140d397677bb707aab51b",
+    "url": "/portfolio/static/media/nextlevel.276c5b05.jpg"
+  },
+  {
+    "revision": "16ffbe0c9b1e49250eac9496735daf5f",
+    "url": "/portfolio/static/media/omnistack.16ffbe0c.jpg"
+  },
+  {
+    "revision": "6a1c04205276f9f5435ad796d5438e69",
+    "url": "/portfolio/static/media/portfolio.6a1c0420.png"
+  },
+  {
+    "revision": "25e5cad88d1bc51da624d72ee94c4383",
+    "url": "/portfolio/static/media/profilePic.25e5cad8.png"
+  },
+  {
+    "revision": "6f41912df530364cd0bf40ec62fb065a",
+    "url": "/portfolio/static/media/react-ionic.6f41912d.png"
+  },
+  {
+    "revision": "ff3510145b80143e64d4089bc02545ad",
+    "url": "/portfolio/static/media/react.ff351014.png"
+  },
+  {
+    "revision": "044658f3d73807f603f6c2b77f11e7cc",
+    "url": "/portfolio/static/media/smart-parking.044658f3.png"
+  },
+  {
+    "revision": "9d55c801a341a697e3d3bd3ec734054e",
+    "url": "/portfolio/static/media/twitter.9d55c801.png"
+  },
+  {
+    "revision": "ed52f187b2c4aad7fd02765c9cdc2f92",
+    "url": "/portfolio/static/media/user.ed52f187.svg"
+  },
+  {
+    "revision": "374563e977822320e68331f9e8ee73b5",
+    "url": "/portfolio/static/media/uxdesign.374563e9.png"
+  },
+  {
+    "revision": "f91ecbcd901a5b2963982de9c22eb714",
+    "url": "/portfolio/static/media/wineer.f91ecbcd.png"
+  }
+]);
